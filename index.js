f2b87ea550e7b346b37a38635c9caf87109fa4b1
charan14092003 <charankallempudi24@gmail.com>
2023-12-10 12:16:53 +0530

    changed initial status

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const replicate = new Replicate({
   auth: process.env.REPLICATE_API_TOKEN,
 });
 app.get("/", (req, res) => {
-  res.json({ msg: "Hello World!" });
+  res.json({ status: 200 });
 });
 
 app.post("/replicate", async (req, res) => {
